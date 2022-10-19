57676cb34374e4f478f36d19afd86195d18b65e0
DenisKa <pochta_dk@mail.ru>
2022-10-20 01:12:50 +0300

    Update task4

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿/*
 // Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
 Console.Write("Input first int number: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 
@@ -24,6 +25,7 @@ else {
 }
 
 //Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
 Console.Write("Input first int number: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 
@@ -33,30 +35,20 @@ int num2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input third int number: ");
 int num3 = Convert.ToInt32(Console.ReadLine());
 
-int max = 0;
+int max = num1;
 
 if ((num1 == num2) & (num2 == num3)) {
-    Console.WriteLine($"{num1} = {num2} = {num3}, same numbers entered");
+    Console.WriteLine($"{num1} = {num2} = {num3}, all three numbers are the same");
 }
 
 else {
-if ((num1 > num2) & (num1 > num3)) {
-    max = num1;
-    Console.WriteLine($"{num1}, {num2}, {num3}, max = {max}, the first number is greater than the second and the third");
-}
-else {
-    if ((num2 > max) & (num2 > num3)) {
-    max = num2;
-    Console.WriteLine($"{num1}, {num2}, {num3}, max = {max}, the second number is greater than the first and the third");
-}
-else {
-    max = num3;
-    Console.WriteLine($"{num1}, {num2}, {num3}, max = {max}, the third number is greater than the first and the second");
-}
-}
+    if (num2 > max) {max = num2;}
+    if (num3 > max) {max = num3;}
+    Console.WriteLine($"{num1}, {num2}, {num3}, max = {max}");
 }
 
 //Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
 Console.Write("Input int number: ");
 int num = Convert.ToInt32(Console.ReadLine());
 
@@ -67,7 +59,7 @@ if (num % 2 == 0) {
 else {
     Console.WriteLine($"{num} - odd number");
 }
-*/
+
 
 //Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 
@@ -82,3 +74,5 @@ while (i <= num) {
     }
     i++;
 }
+
+*/
