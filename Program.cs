7fbcd7f0006c9d16af3aacb855f05eb2e466e466
DenisKa <pochta_dk@mail.ru>
2022-10-19 23:29:16 +0300

    Add task2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.Write("Input first int number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input second int number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int max = 0;
+
+if (num1 == num2) {
+    Console.WriteLine($"{num1} = {num2}, same numbers entered");
+}
+
+else {
+if (num1 > num2) {
+    max = num1;
+    Console.WriteLine($"{num1} > {num2}, max = {max}, the first number is greater than the second");
+}
+else {
+    max = num2;
+    Console.WriteLine($"{num1} < {num2}, max = {max}, the second number is greater than the first");
+}
+}
+
+
+//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+
+
+//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+
+
+//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
